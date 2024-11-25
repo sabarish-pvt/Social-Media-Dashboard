f7a078ad1ce20dbee8127990395a21185b76be8b
sabarish-pvt <sabarishsabarish540@gmail.com>
2024-11-25 12:45:22 +0530

    Add files via upload

---
+++ darkmodetoggle.js
@@ -0,0 +1,10 @@
+// Function to toggle between themes
+document.getElementById('toggle').addEventListener('change', function () {
+  if (this.checked) {
+    document.body.classList.remove('light-theme');
+    document.body.classList.add('dark-theme');
+  } else {
+    document.body.classList.remove('dark-theme');
+    document.body.classList.add('light-theme');
+  }
+});
